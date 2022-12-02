f5a304d661e524ea432ed03211c916da33fca673
ari <ariefendi992@gmail.com>
2022-12-02 10:37:11 +0800

    update animation

--- animation.py
+++ animation.py
@@ -89,27 +89,62 @@ def main(page: Page):
     # page.add(c, ElevatedButton("Reveal!", on_click=animate))
 
     # Position animation
-    c1 = Container(width=50, height=50, bgcolor="red", animate_position=1000)
-    c2 = Container(
-        width=50, height=50, bgcolor="green", top=60, left=0, animate_position=500
-    )
-    c3 = Container(
-        width=50, height=50, bgcolor="blue", top=120, left=0, animate_position=1000
+    # c1 = Container(width=50, height=50, bgcolor="red", animate_position=1000)
+    # c2 = Container(
+    #     width=50, height=50, bgcolor="green", top=60, left=0, animate_position=500
+    # )
+    # c3 = Container(
+    #     width=50, height=50, bgcolor="blue", top=120, left=0, animate_position=1000
+    # )
+
+    # def animate_container(e):
+    #     c1.top = 20
+    #     c1.left = 200
+    #     c2.top = 100
+    #     c2.left = 40
+    #     c3.top = 180
+    #     c3.left = 100
+    #     page.update()
+
+    # page.add(
+    #     Stack([c1, c2, c3], height=250),
+    #     ElevatedButton("Animate!", on_click=animate_container),
+    # )
+
+    # animate container
+    # c = Container(
+    #     width=150,
+    #     height=150,
+    #     bgcolor="red",
+    #     animate=animation.Animation(1000, AnimationCurve.BOUNCE_OUT),
+    # )
+
+    # def animate_container(e):
+    #     c.width = 100 if c.width == 150 else 150
+    #     c.height = 50 if c.height == 150 else 150
+    #     c.bgcolor = "blue" if c.bgcolor == "red" else "red"
+    #     c.update()
+
+    # page.add(c, ElevatedButton("Animate Container", on_click=animate_container))
+
+    # Animated content switcher
+    i = Image(src="https://picsum.photos/150/150", width=150, height=150)
+
+    sw = AnimatedSwitcher(
+        i,
+        transition=AnimatedSwitcherTransition.SCALE,
+        duration=500,
+        switch_in_curve=AnimationCurve.BOUNCE_OUT,
+        switch_out_curve=AnimationCurve.BOUNCE_IN,
     )
 
-    def animate_container(e):
-        c1.top = 20
-        c1.left = 200
-        c2.top = 100
-        c2.left = 40
-        c3.top = 180
-        c3.left = 100
+    def animate(e):
+        sw.content = Image(
+            src=f"https://picsum.photos/150/150?{time.time()}", width=150, height=150
+        )
         page.update()
 
-    page.add(
-        Stack([c1, c2, c3], height=250),
-        ElevatedButton("Animate!", on_click=animate_container),
-    )
+    page.add(sw, ElevatedButton("Animate!", on_click=animate))
 
 
 ft.app(target=main)
